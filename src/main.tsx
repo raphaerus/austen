e0a6353b9970aa646b362660e7968a0d16f7a3fa
raphaerus <raphaelwebdev.fronted@gmail.com>
2026-04-20 11:58:27 -0300

    UI: corrige title tag da aplicacao e vincula ao data-config paramétrico

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SITE_CONFIG } from './config/siteData.ts'
+
+document.title = SITE_CONFIG.company.name;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
